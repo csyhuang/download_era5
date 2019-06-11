1261083ace709ffc8e89583b1923a8612e9ae991
csyhuang <csyhuang@uchicago.edu>
2019-06-10 20:03:08 -0500

    trial

--- trial_download.py
+++ trial_download.py
@@ -1,49 +1,19 @@
+#!/usr/bin/env python
 import cdsapi
-
+ 
 c = cdsapi.Client()
-
-c.retrieve(
-    'reanalysis-era5-pressure-levels',
-    {
-        'pressure_level':[
-            '1','2','3',
-            '5','7','10',
-            '20','30','50',
-            '70','100','125',
-            '150','175','200',
-            '225','250','300',
-            '350','400','450',
-            '500','550','600',
-            '650','700','750',
-            '775','800','825',
-            '850','875','900',
-            '925','950','975',
-            '1000'
-        ],
-        'variable':[
-            'geopotential','temperature','u_component_of_wind',
-            'v_component_of_wind','vertical_velocity'
-        ],
-        'time':[
-            '00:00','06:00','12:00',
-            '18:00'
-        ],
-        'product_type':'reanalysis',
-        'year':'1979',
-        'day':[
-            '01','02','03',
-            '04','05','06',
-            '07','08','09',
-            '10','11','12',
-            '13','14','15',
-            '16','17','18',
-            '19','20','21',
-            '22','23','24',
-            '25','26','27',
-            '28','29','30',
-            '31'
-        ],
-        'month':'01',
-        'format':'netcdf'
-    },
-    'download.nc')
+c.retrieve('reanalysis-era5-complete', {
+    'class'   : 'ea',
+    'expver'  : '1',
+    'stream'  : 'oper',
+    'type'    : 'fc',
+    'step'    : '3/to/12/by/3',
+    'param'   : '130.128',
+    'levtype' : 'ml',
+    'levelist': '1/10/50/100',
+    'date'    : '2013-01-01',
+    'time'    : '06/18',
+    'area'    : '80/-50/-25/0', # North, West, South, East. Default: global
+    'grid'    : '1.0/1.0', # Latitude/longitude grid: east-west (longitude) and north-south resolution (latitude). Default: reduced Gaussian grid
+    'format'  : 'netcdf', # Default: grib
+}, 'temp-fc-ml.nc')
